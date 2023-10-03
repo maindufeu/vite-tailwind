c39f354f3c85d4f7dec7edcb3310301836455b1f
Edher <ejimenezp@aeromexico.com>
2023-10-03 17:45:35 -0600

    tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ]
 }
